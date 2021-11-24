bd640d1c482c2abfe6eb0c7ea81fff90877030d8
Chaucer <sqa13@sfu.ca>
2021-11-23 19:03:39 -0800

    testdata

--- test.py
+++ test.py
@@ -9,6 +9,9 @@ import seaborn as sns
 from matplotlib.ticker import FuncFormatter
 import matplotlib.font_manager
 from sklearn.ensemble import RandomForestRegressor
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
 
 plt.rcParams['font.sans-serif']=['Arial Unicode MS']
 
@@ -24,6 +27,15 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 df_gift_card = pd.DataFrame()
+
+conf = SparkConf()
+conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')
+conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
+ 
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
+ 
+df = spark.read.csv('s3a://cmpt732-12cobblers/', inferSchema=True)
+
 
 with open(PATH_project) as f:
     for i in f:
