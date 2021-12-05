fcc453901dd15ff288d964d6141d8c4f6b3c2cf0
Chaucer <sqa13@sfu.ca>
2021-12-05 04:17:50 -0800

    final

--- Customer_IfPurchasement.py
+++ Customer_IfPurchasement.py
@@ -1,13 +1,33 @@
 import pandas as pd
 from sklearn import svm
 import joblib
-from sklearn import metrics,model_selection
+from sklearn import metrics,model_selection,preprocessing
 import os
 import sys
-
+import joblib
 '''
 Input ==> Json file name got from Customer_IfPurchasement_Data_Preparation.py
+    +---------------------+-----------+--------------------+-------------+------------+-------------+-----------+----+-----------------+
+    |Product_Main_Category|Cat_Encoder|       Product_Brand|Brand_Encoder|Product_Rank|Product_Price|Review_Vote|Rate|Product_Purchased|
+    +---------------------+-----------+--------------------+-------------+------------+-------------+-----------+----+-----------------+
+    |            Computers|       13.0|                Dell|       3961.0|     14269.0|         76.4|        0.0| 3.0|                1|
+    |            Computers|       13.0|          Easy Style|       4606.0|     21056.0|        24.47|        0.0| 5.0|                1|
+    |            Computers|       13.0|             Samsung|      13084.0|     10109.0|       399.95|        7.0| 1.0|               -1|
+    |            Computers|       13.0|                 ZTC|      16827.0|       290.0|         0.37|        0.0| 4.0|                1|
+    |            Computers|       13.0|             ProCase|      11853.0|        95.0|        15.99|        0.0| 5.0|                1|
+    |            Computers|       13.0|SilverStone Techn...|      13485.0|      3854.0|         2.54|        0.0| 5.0|                1|
+    |            Computers|       13.0|                ZAGG|      16792.0|       188.0|        54.99|        0.0| 3.0|                1|
+    |            Computers|       13.0|           Transcend|      15198.0|       656.0|        13.99|        0.0| 5.0|                1|
+    |            Computers|       13.0|           Medialink|       9657.0|      8466.0|         6.31|        3.0| 1.0|                1|
+    |            Computers|       13.0|       Cooler Master|       3324.0|       383.0|        15.92|        0.0| 1.0|                1|
+    |            Computers|       13.0|             Samsung|      13084.0|     10109.0|       399.95|        0.0| 5.0|                1|
+    |            Computers|       13.0|      Bargains Depot|       1618.0|      2176.0|         6.99|        0.0| 5.0|               -1|
+    |            Computers|       13.0|               Wacom|      16116.0|      6264.0|         4.07|        0.0| 3.0|                1|
+    +---------------------+-----------+--------------------+-------------+------------+-------------+-----------+----+-----------------+
+
 Output ==> SVM Model for prediction
+
+Make Prediction require read two encoders json to build up data
 '''
 def read_data(Path):
     data = pd.read_json(Path,lines=True)
@@ -20,25 +40,30 @@ def read_data(Path):
     Y = data["Product_Purchased"]
     X = X.to_numpy()
     Y = Y.to_numpy()
-    x_train, x_test, y_train, y_test = model_selection.train_test_split(X,Y, train_size=0.98, test_size=0.02)
-    x_train, x_val, y_train, y_val = model_selection.train_test_split(x_train, y_train, train_size=0.95, test_size=0.05)
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(X,Y, train_size=0.95, test_size=0.05)
+    min_max_scaler = preprocessing.MinMaxScaler()
+    
+    x_train = min_max_scaler.fit_transform(x_train)
+    x_test = min_max_scaler.fit_transform(x_test)
 
-    return x_train, y_train, x_val, y_val, x_test, y_test
+    return x_train, y_train, x_test, y_test
 
 
 def main(Path):
 
-    x_train, y_train, x_val, y_val, x_test, y_test = read_data(Path)
-    model = svm.SVC(C = 1.0, kernel='rbf', gamma='auto', decision_function_shape='ovr', cache_size=500, random_state = 1000)
+    x_train, y_train, x_test, y_test = read_data(Path)
+    model = svm.SVC(C = 1.0, kernel='rbf', gamma='auto', decision_function_shape='ovr', cache_size=1000, random_state = 1000, max_iter=5000)
     model.fit(x_train, y_train)
-    print("Train set score" + str(model.score(x_train, y_train)))
-    print("Validation set score" + str(model.score(x_val, y_val)))
+    print("Train set accuracy" + str(model.score(x_train, y_train)))
+    print("Test set accuracy" + str(model.score(x_test, y_test)))
 
     tn_train, fp_train, fn_train, tp_train = metrics.confusion_matrix(y_train, model.predict(x_train)).ravel()
-    tn_val, fp_val, fn_val, tp_val = metrics.confusion_matrix(y_val, model.predict(x_val)).ravel()
+    tn_val, fp_val, fn_val, tp_val = metrics.confusion_matrix(y_test, model.predict(x_test)).ravel()
 
     print("Train positive correct number is {}, train positive wrong number is {}.\n Train negative correct number is {}, train negative wrong number is {}".format(tp_train, fp_train, tn_train, fn_train))
-    print("Validation positive correct number is {}, validation positive wrong number is {}.\n Validation negative correct number is {}, validation negative wrong number is {}".format(tp_val, fp_val, tn_val, fn_val))
+    print("Test positive correct number is {}, test positive wrong number is {}.\n Test negative correct number is {}, test negative wrong number is {}".format(tp_val, fp_val, tn_val, fn_val))
+
+    _ = joblib.dump(model, os.path.join(ffolder, "Customer_IfPurchasement_SVM_Model"))
 
 if __name__ == '__main__':
     ffolder = os.path.abspath("")
